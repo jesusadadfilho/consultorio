cb175a6a4036de599a5cbf21d722e9bfc410735c
MicaelLeal <mycaellmoura@hotmail.com>
2018-12-08 03:39:46 -0300

    Definição final dos modelos

--- classes/models.py
+++ classes/models.py
@@ -1,42 +1,58 @@
 from django.db import models
 
 class Especialidade(models.Model):
+
      nome = models.CharField(max_length=30)
      descricao = models.CharField(max_length=100)
-
-class Atendente(models.Model):
-    nome =  models.CharField(max_length=30)
-    dt_nascimento = models.DateField()
-    CPF =  models.CharField(max_length=30)
-
-class Cliente(models.Model):
-    nome = models.CharField(max_length=30)
-    dt_nascimento = models.DateField()
-    CPF = models.CharField(max_length=30)
-    sexo = (
-        ('M', 'masculino'),
-        ('F', 'feminino')
-    )
-    sexo = models.CharField(max_length=1, choices=sexo)
 
 
 class Medico(models.Model):
     nome = models.CharField(max_length=30)
+    CPF = models.CharField(max_length=14)
     dt_nascimento = models.DateField()
-    CPF = models.CharField(max_length=30)
-    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE,related_name="medicos")
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE,
+                                      related_name="medicos")
+
+
+class Atendente(models.Model):
+
+    nome = models.CharField(max_length=30)
+    CPF =  models.CharField(max_length=14)
+    dt_nascimento = models.DateField()
+
+
+class Cliente(models.Model):
+
+    nome = models.CharField(max_length=30)
+    CPF = models.CharField(max_length=14)
+    dt_nascimento = models.DateField()
+    SEXO = (
+        ('M', 'masculino'),
+        ('F', 'feminino')
+    )
+    sexo = models.CharField(max_length=1, choices=SEXO)
+
 
 class Agendamento(models.Model):
-    dt_agendamento = models.DateField()
-    dt_agendada = models.DateField()
-    atendente = models.ForeignKey(Atendente, on_delete=models.CASCADE,related_name="agendamentos")
-    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE,related_name="agendamentos")
+
+    dt_agendamento = models.DateTimeField()
+    dt_agendada = models.DateTimeField()
+    cliente = models.ForeignKey(Cliente, related_name='agendamentos',
+                                on_delete=models.CASCADE)
+    atendente = models.ForeignKey(Atendente, on_delete=models.CASCADE,
+                                  related_name="agendamentos")
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE,
+                                      related_name="agendamentos")
+
 
 class Consulta(models.Model):
-    dt_consulta = models.DateField
-    agendamento =  models.OneToOneField(Agendamento, on_delete=models.CASCADE,related_name="Consulta")
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE,related_name="consultas")
-    medico = models.ForeignKey(Medico, on_delete=models.CASCADE,related_name="consultas")
-    retorno = models.ManyToManyField("self")
 
+    dt_consulta = models.DateTimeField()
+    diagnostico = models.CharField(max_length=100)
+    agendamento = models.OneToOneField(Agendamento, on_delete=models.CASCADE,
+                                       related_name="Consulta")
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE,
+                                related_name="consultas")
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE,
+                               related_name="consultas")
 
